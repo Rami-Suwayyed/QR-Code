8b6d54fb8cc9ae3f39d2a5478de80631f6370415
Rami-Suwayyed <swead96@gmail.com>
2022-06-24 22:36:38 +0300

    QR Code Attachment

---
+++ resources/views/qr_code/qr_geo.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header d-flex">
+                        {{ __('QR Builder') }}
+                        <div class="ml-auto">
+                            <a href="{{ route('qr_builder') }}">QR Builder</a> -
+                            <a href="{{ route('qr_phone') }}">Phone</a> -
+                            <a href="{{ route('qr_email') }}">Email</a> -
+                            <a href="{{ route('qr_geo') }}">Maps</a> -
+                            <a href="{{ route('qr_sms') }}">SMS</a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <div class="text-center">
+
+                            {!! QrCode::geo(31.84863996504318, 35.918172155301846) !!}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
